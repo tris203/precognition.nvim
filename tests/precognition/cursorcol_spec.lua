70eb6e3ea7f3a238c442e7ce4f91a38c7ec65d6c
bokonist <bokonist@users.noreply.github.com>
2025-06-08 16:50:58 +0530

    added tests for cursorcol calculation

---
+++ tests/precognition/cursorcol_spec.lua
@@ -0,0 +1,43 @@
+local precognition = require("precognition")
+local motions = require("precognition.motions").get_motions()
+local utils = require("precognition.utils")
+---@diagnostic disable-next-line: undefined-field
+local eq = assert.are.same
+
+local tabstop = 4
+
+describe("Calculate current cursorcol position", function()
+    it("calc current cursor col position for an empty string", function()
+        local whitespace = ""
+        local current_line = whitespace .. ""
+        local num_tabs = select(2, string.gsub(whitespace, "\t", "")) -- number of tabs in the whitespace we're testing for
+        local cursorposition = #whitespace + 1 -- empty lines would just place cursor on first col
+        local cursorcol = precognition.calculate_cursorcol(current_line, cursorposition, tabstop)
+        eq(cursorcol, cursorposition + tabstop * num_tabs - num_tabs)
+    end)
+    it("calculate col for leading spaces only", function()
+        local whitespace = "    "
+        local current_line = whitespace .. "word1 word2 word3"
+        local num_tabs = select(2, string.gsub(whitespace, "\t", ""))
+        local cursorposition = #whitespace + 1 -- place the initial cursor on the first char of the first word
+        local cursorcol = precognition.calculate_cursorcol(current_line, cursorposition, tabstop)
+        eq(cursorcol, cursorposition + tabstop * num_tabs - num_tabs)
+    end)
+    it("calculate col for leading tabs only", function()
+        local whitespace = "\t\t\t"
+        local current_line = whitespace .. "word1 word2 word3"
+        local num_tabs = select(2, string.gsub(whitespace, "\t", ""))
+        local cursorposition = #whitespace + 1
+        local cursorcol = precognition.calculate_cursorcol(current_line, cursorposition, tabstop)
+        eq(cursorcol, cursorposition + tabstop * num_tabs - num_tabs)
+    end)
+    it("calculate col for leading mix of spaces and tabs in any order", function()
+        local whitespace = "  \t \t  \t \t  \t"
+        local current_line = whitespace .. "word1 word2 word3"
+
+        local num_tabs = select(2, string.gsub(whitespace, "\t", ""))
+        local cursorposition = #whitespace + 1
+        local cursorcol = precognition.calculate_cursorcol(current_line, cursorposition, tabstop)
+        eq(cursorcol, cursorposition + tabstop * num_tabs - num_tabs)
+    end)
+end)
